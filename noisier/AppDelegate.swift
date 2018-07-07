848506da63ea43d2ce5c9274d48b986df7b606ce
Kari Silvennoinen <kari@kalifi.org>
2018-07-07 21:23:55 +0300

    Initial Commit

---
+++ noisier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  noisier
+//
+//  Created by Kari Silvennoinen on 07.07.2018.
+//  Copyright © 2018 Kalifi.org. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
